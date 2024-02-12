224a04a31d96518d789c7aadf7bed758279851a6
firdayulianti <yuliantifirda966@gmail.com>
2024-02-12 11:28:53 +0700

    update

---
+++ jscroot/controller/map/template.js
@@ -0,0 +1,16 @@
+export let xxx = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/pirda-getjson";
+export let aaa = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/yuliana-postpoint";
+export let bbb = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/yuliana-postlinestring";
+export let ccc = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/pirda-postpolygon";
+export let qqq = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/yulianaIntersect";
+export let www = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/pirdaWithin";
+export let eee = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/yulianaNear";
+export let rrr = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/pirdaNearsphere";
+export let ttt = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/pirdaBox";
+export let yyy = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/yulianaCenter";
+export let uuu = "/postcentersphere";
+
+export let clickpopup = `
+Coordinate  : #COORDINATE#<br>
+Type  : #TYPE#<br>
+`;
